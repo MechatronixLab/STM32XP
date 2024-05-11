97d3401769f31ee740db57542db01aacc0e40fc0
André Araújo <94403601+andremdaraujo@users.noreply.github.com>
2024-05-11 08:13:41 -0300

    Firmware project creation for breadboard prototype.

---
+++ FIRMWARE/STM32XP_PROTOTYPE_BREADBOARD/App/Src/app.c
@@ -0,0 +1,24 @@
+/*
+ * app.c
+ *
+ *  Created on: May 11, 2024
+ *      Author: Eng. André A. M. Araújo
+ */
+
+#include "app.h"
+
+void APP_Init(void)
+{
+	APP_Run();
+}
+
+void APP_Run(void)
+{
+	while (1)
+	{
+		HAL_GPIO_TogglePin(OLED_RST_GPIO_Port, OLED_RST_Pin);
+		HAL_GPIO_TogglePin(LED_USER_GPIO_Port, LED_USER_Pin);
+		HAL_Delay(250);
+	}
+}
+
